bac8f52f32ed2401e4e2a17068f48a9df55d5a4a
Neelay Junnarkar <neelay.junnarkar@gmail.com>
2023-10-25 17:02:44 -0700

    modified inverted pendulum and related files

--- environments.py
+++ environments.py
@@ -1,15 +1,21 @@
-from gym.envs.mujoco.inverted_pendulum_v4 import InvertedPendulumEnv
+import gym
+from gym.envs.mujoco.inverted_pendulum import InvertedPendulumEnv # gym 23.1 gives InvertedPendulum v2
+# from gym.envs.mujoco.inverted_pendulum_v4 import InvertedPendulumEnv
 import numpy as np
 
 class RewardAssymetricInvertedPendulum(InvertedPendulumEnv):
-    goal_x = 10.0
+    goal_x = 1.0
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
     def step(self, action):
         obs, _reward, done, info = super().step(action)
-        b = 100
-        c = 1.03
         diff = obs[0] - self.goal_x
+        b = 2
+        c = 1.5
         reward = b/(np.power(c, diff) + np.power(c, -diff))
+        # reward = -diff**2
+
+        # notdone = np.isfinite(obs).all() and (np.abs(obs[1]) <= 0.2) # Changed angle threshold to 0.75rad from 0.2rad
+        # done = not notdone
         return obs, reward, done, info
---
+++ gather_data.py
@@ -0,0 +1,59 @@
+import d3rlpy
+from d3rlpy.algos import COMBO
+from sklearn.model_selection import train_test_split
+#import gymnasium as gym
+import gym
+from gym.wrappers import TransformObservation, TimeLimit
+import numpy as np
+import encoders
+import os
+import json
+import environments
+print(gym.version.VERSION)
+
+seed = 1
+d3rlpy.seed(seed)
+use_gpu = True
+
+env = TimeLimit(environments.RewardAssymetricInvertedPendulum(), max_episode_steps=1000)
+eval_env = TimeLimit(environments.RewardAssymetricInvertedPendulum(), max_episode_steps=1000)
+
+env.reset(seed=seed)
+eval_env.reset(seed=seed)
+
+actor_encoder = d3rlpy.models.encoders.DefaultEncoderFactory(dropout_rate=0.2)
+# setup algorithm
+sac = d3rlpy.algos.SAC(
+    batch_size=256,
+    actor_encoder_factory=actor_encoder,
+    actor_learning_rate=1e-3,
+    critic_learning_rate=1e-3,
+    temp_learning_rate=1e-3,
+    use_gpu=use_gpu
+)
+
+# prepare utilities
+buffer = d3rlpy.online.buffers.ReplayBuffer(maxlen=100000, env=env)
+
+# start training
+sac.fit_online(
+    env,
+    buffer,
+    eval_env=eval_env,
+    n_steps=100000,
+    n_steps_per_epoch=1000,
+    update_interval=2,
+    update_start_step=1000,
+    tensorboard_dir='tensorboard_logs',
+    experiment_name='test7'
+)
+
+dataset = buffer.to_mdp_dataset()
+dataset.dump('d3rlpy_data/rwd_assym_inv_pend_v7.h5')
+
+scorer = d3rlpy.metrics.scorer.evaluate_on_environment(eval_env, render=True)
+mean_episode_return = scorer(sac)
+
+# 5: goal 0, lr 3e-4, update interval 2
+# 6: goal 1, lr 3e-4, update interval 2
+# 7: goal 1, lr 1e-3, update interval 2, time limit 1000
