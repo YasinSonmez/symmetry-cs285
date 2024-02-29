94df93d5da40bad4e4b2e9fd73a67f71b3d9e262
Neelay Junnarkar <neelay.junnarkar@gmail.com>
2024-02-28 17:09:40 -0800

    minor sbatch script change

--- submit_experiments_two_car.sh
+++ submit_experiments_two_car.sh
@@ -8,7 +8,7 @@
 # Always at least twice the number of GPUs (savio2_gpu and GTX2080TI in savio3_gpu)
 # Four times the number for TITAN and V100 in savio3_gpu and A5000 in savio4_gpu
 # Eight times the number for A40 in savio3_gpu
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=2
 #Number of GPUs, this can be in the format of "gpu:[1-4]", or "gpu:K80:[1-4] with the type included
 #SBATCH --gres=gpu:GTX2080TI:1
 #SBATCH --time=48:00:00
